5dec8eb384d7675e2914eb3dda27dd5f8eb49660
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-09-08 16:50:03 -0300

    Injecao de dependencias(service)

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -10,30 +11,9 @@ export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
     
+    constructor(private courseService: CourseService){}
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png', 
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate:'May, 2, 2021'
-            
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png', 
-                price: 45.99,
-                code: 'LKL-3521',
-                duration: 80,
-                rating: 3.5,
-                releaseDate:'July, 14, 2021'
-            
-            }
-        ]
+      this.courses = this.courseService.retriveAll();
     }
 }
