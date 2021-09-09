bd2c0dad6e87fcd40a8bf8e9ac4b80785dd5b1f6
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-09-09 20:13:21 -0300

    add pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { StarComponent } from './star/star.component';
-import { FormsModule } from '@angular/forms';
+import { ReplacePipe } from './pipes/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
